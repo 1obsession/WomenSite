0dda68f1b34dc0e80867924849c93f334129765f
Vladimir Pavlov <proiqiz@gmail.com>
2025-05-09 19:46:07 +0300

    Добавил профиль

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 
 class LoginUserForm(AuthenticationForm):
@@ -14,24 +14,23 @@ class LoginUserForm(AuthenticationForm):
         fields = ['username', 'password']
 
 
-class RegisterUserForm(forms.ModelForm):
-    username = forms.CharField(label='Логин')
-    password = forms.CharField(label='Пароль', widget=forms.PasswordInput())
-    password2 = forms.CharField(label='Повтор пароля', widget=forms.PasswordInput())
+class RegisterUserForm(UserCreationForm):
+    username = forms.CharField(label='Логин', widget=forms.TextInput(attrs={'class': 'form-input'}))
+    password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+    password2 = forms.CharField(label='Повтор пароля', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
 
     class Meta:
         model = get_user_model()
-        fields = ['username', 'email', 'first_name', 'last_name', 'password', 'password2']
+        fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']
         labels = {'first_name': 'Имя',
                   'last_name': 'Фамилия',
                   'email': 'E-mail',
                   }
+        widgets = {'first_name': forms.TextInput(attrs={'class': 'form-input'}),
+                  'last_name': forms.TextInput(attrs={'class': 'form-input'}),
+                  'email': forms.TextInput(attrs={'class': 'form-input'}),
 
-    def clean_password2(self):
-        cd = self.cleaned_data
-        if cd['password'] != cd['password2']:
-            raise forms.ValidationError('Пароли не совпадают!')
-        return cd['password2']
+        }
 
     def clean_email(self):
         email = self.cleaned_data['email']
@@ -39,4 +38,17 @@ class RegisterUserForm(forms.ModelForm):
             raise forms.ValidationError('Такой email уже существует')
         return email
 
-    
+
+class ProfileUserForm(forms.ModelForm):
+    username = forms.CharField(disabled=True, label='Логин', widget=forms.TextInput(attrs={'class': 'form-input'}))
+    email = forms.CharField(disabled=True, label='E-mail', widget=forms.TextInput(attrs={'class': 'form-input'}))
+
+    class Meta:
+        model = get_user_model()
+        fields = ['username', 'email', 'first_name', 'last_name']
+        labels = {'first_name': 'Имя',
+                  'last_name': "Фамилия",
+                  }
+        widgets = {'first_name': forms.TextInput(attrs={'class': 'form-input'}),
+                   'last_name': forms.TextInput(attrs={'class': 'form-input'}),
+                   }
--- users/views.py
+++ users/views.py
@@ -1,10 +1,12 @@
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login, logout, get_user_model
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView, UpdateView
 
-from .forms import LoginUserForm, RegisterUserForm
+from .forms import LoginUserForm, RegisterUserForm, ProfileUserForm
 
 
 class LoginUser(LoginView):
@@ -13,14 +15,29 @@ class LoginUser(LoginView):
     extra_context = {'title': 'Авторизация'}
 
 
-def register(request):
-    if request.method == "POST":
-        form = RegisterUserForm(request.POST)
-        if form.is_valid():
-            user = form.save(commit=False)
-            user.set_password(form.cleaned_data['password'])
-            user.save()
-            return render(request, 'users/register_done.html')
-    else:
-        form = RegisterUserForm()
-    return render(request, 'users/register.html', {'form': form})
+class ProfileUser(LoginRequiredMixin, UpdateView):
+    form_class = ProfileUserForm
+    template_name = 'users/profile.html'
+    extra_context = {'title': 'Профиль пользователя'}
+    success_url = reverse_lazy('users:profile')
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
+class UserRegister(CreateView):
+    form_class = RegisterUserForm
+    template_name = 'users/register.html'
+    extra_context = {'title': 'Регистрация'}
+    success_url = reverse_lazy('users:login')
+
+# def register(request):
+#     if request.method == "POST":
+#         form = RegisterUserForm(request.POST)
+#         if form.is_valid():
+#             user = form.save(commit=False)
+#             user.set_password(form.cleaned_data['password'])
+#             user.save()
+#             return render(request, 'users/register_done.html')
+#     else:
+#         form = RegisterUserForm()
+#     return render(request, 'users/register.html', {'form': form})
